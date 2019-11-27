b4ea0ed2cf3872b50583d03aae9227f16ac4521f
Daniel Pauli <dapaulid@gmail.com>
2019-11-27 20:59:13 +0100

    Add failure tests.

---
+++ test/failure.test.cpp
@@ -0,0 +1,41 @@
+#include "test.h"
+
+#include "local_endpoint.h"
+#include "remote_endpoint.h"
+#include "types.h" // TypedValue
+
+//------------------------------------------------------------------------------
+//
+TEST(Failure, bind_with_bad_name)
+{
+    remo::LocalEndpoint endpoint;
+    
+    try {
+        endpoint.bind("name with space", [](){});
+        FAIL() << "must throw an exception";
+    } catch (const remo::error& e) {
+        EXPECT_EQ(e.code(), remo::ErrorCode::ERR_INVALID_IDENTIFIER);
+    }
+}
+
+//------------------------------------------------------------------------------
+//
+TEST(Failure, call_with_param_type)
+{
+    remo::LocalEndpoint endpoint;
+    remo::RemoteEndpoint* remote = endpoint.connect(".");
+
+    endpoint.bind("testfunc", [](uint32_t){});
+
+    try {
+        remote->call("testfunc", 1234.5678);
+        FAIL() << "must throw an exception";
+    } catch (const remo::error& e) {
+        EXPECT_EQ(e.code(), remo::ErrorCode::ERR_PARAM_TYPE_MISMATCH);
+    }
+}
+
+
+//------------------------------------------------------------------------------
+// end of file
+//------------------------------------------------------------------------------
