aa4b08008f54baf7af2d20905b729e71705d3168
Daniel Pauli <dapaulid@gmail.com>
2019-12-01 13:37:40 +0100

    Disable branch coverage.

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -2,15 +2,16 @@
 
 BUILDTYPE="Debug"
 BUILDDIR="build/$BUILDTYPE"
+#LCOV_ARGS="--rc lcov_branch_coverage=1"
 
 echo -e "\e[1;36m================================================================================\e[0m"
 echo -e "\e[1;36m                              C O V E R A G E                                   \e[0m"
 echo -e "\e[1;36m================================================================================\e[0m"
 cd $BUILDDIR
 # capture coverage
-lcov --capture --directory src --directory test --output-file coverage.info --quiet --rc lcov_branch_coverage=1
+lcov --capture --directory src --directory test --output-file coverage.info --quiet $LCOV_ARGS
 # only keep info about files in src directory
 # necessary to correctly handle header files
-lcov --extract coverage.info '*/src/*' --output-file coverage.info --quiet --rc lcov_branch_coverage=1
+lcov --extract coverage.info '*/src/*' --output-file coverage.info --quiet $LCOV_ARGS
 # output summary
-lcov --list coverage.info --rc lcov_branch_coverage=1
+lcov --list coverage.info $LCOV_ARGS
