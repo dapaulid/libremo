6fa4cb910a9532750a70fab0bf677c8b396d6a1d
Daniel Pauli <dapaulid@gmail.com>
2019-12-01 11:32:16 +0100

    Only include src files in coverage report.

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -7,5 +7,10 @@ echo -e "\e[1;36m===============================================================
 echo -e "\e[1;36m                              C O V E R A G E                                   \e[0m"
 echo -e "\e[1;36m================================================================================\e[0m"
 cd $BUILDDIR
-lcov --capture --quiet --directory src --directory test --output-file coverage.info --rc lcov_branch_coverage=1
-lcov --summary coverage.info --rc lcov_branch_coverage=1
+# capture coverage
+lcov --capture --directory src --directory test --output-file coverage.info --quiet --rc lcov_branch_coverage=1
+# only keep info about files in src directory
+# necessary to correctly handle header files
+lcov --extract coverage.info '*/src/*' --output-file coverage.info --quiet --rc lcov_branch_coverage=1
+# output summary
+lcov --list coverage.info --rc lcov_branch_coverage=1
