0e239d0cc4c07b0debdebac5d919cd21203543cc
Daniel Pauli <dapaulid@gmail.com>
2020-02-11 21:17:15 +0100

    Fix Windows run script.

--- run.cmd
+++ run.cmd
@@ -1,12 +1,12 @@
 @echo off
 
 if not exist build (
-	ci\prebuild.cmd || goto :failed
+	call ci\prebuild.cmd || goto :failed
 )
-ci\build.cmd || goto :failed
-ci\test.cmd || goto :failed
-ci\coverage.cmd || goto :failed
-ci\example.cmd || goto :failed
+call ci\build.cmd || goto :failed
+call ci\test.cmd || goto :failed
+call ci\coverage.cmd || goto :failed
+call ci\example.cmd || goto :failed
 
 :success
 goto :EOF
