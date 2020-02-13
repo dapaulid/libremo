170ea331da0f74103de8f744fbb109b627066f4f
Daniel Pauli <dapaulid@gmail.com>
2020-02-13 22:35:14 +0100

    Add colors for logger.

--- src/l0_system/logger.cpp
+++ src/l0_system/logger.cpp
@@ -14,6 +14,82 @@
 #include <sstream> // stringstream
 #include <iostream>
 
+//------------------------------------------------------------------------------
+// helpers
+//------------------------------------------------------------------------------	
+//
+namespace color {
+	enum class Color {
+		none           = 0,
+
+		black          = 30,
+		red            = 31,
+		green          = 32,
+		yellow         = 33,
+		blue           = 34,
+		magenta        = 35,
+		cyan           = 36,
+		white          = 37,
+
+		bright_black   = 90,
+		bright_red     = 91,
+		bright_green   = 92,
+		bright_yellow  = 93,
+		bright_blue    = 94,
+		bright_magenta = 95,
+		bright_cyan    = 96,
+		bright_white   = 97,
+	};
+
+	enum class Style {
+		none           = 0,
+		bold           = 1,
+		underline      = 4
+	};
+
+	std::string colorize(const std::string& a_str, Color a_forecol, Color a_backcol = Color::none, Style a_style = Style::none)
+	{
+		if (a_forecol != Color::none || a_backcol != Color::none || a_style != Style::none) {
+			std::string colorized("\x1B[");
+			const char* sep = "";
+			if (a_forecol != Color::none) {
+				colorized += std::to_string((int)a_forecol);
+				sep = ";";
+			}
+			if (a_backcol != Color::none) {
+				colorized += sep + std::to_string((int)a_backcol + 10);
+				sep = ";";
+			}
+			if (a_style != Style::none) {
+				colorized += sep + std::to_string((int)a_style);
+				sep = ";";
+			}
+			colorized += "m" + a_str + "\x1B[0m";
+			return colorized;
+		} else {
+			return a_str;
+		}
+	}
+
+	#define DECL_COLOR_FUNC(COL) std::string COL(const std::string& s, Style style = Style::none) { return colorize(s, Color::COL, Color::none, style); }
+	DECL_COLOR_FUNC(black)
+	DECL_COLOR_FUNC(red)
+	DECL_COLOR_FUNC(green)
+	DECL_COLOR_FUNC(yellow)
+	DECL_COLOR_FUNC(blue)
+	DECL_COLOR_FUNC(magenta)
+	DECL_COLOR_FUNC(cyan)
+	DECL_COLOR_FUNC(white)
+	DECL_COLOR_FUNC(bright_black)
+	DECL_COLOR_FUNC(bright_red)
+	DECL_COLOR_FUNC(bright_green)
+	DECL_COLOR_FUNC(bright_yellow)
+	DECL_COLOR_FUNC(bright_blue)
+	DECL_COLOR_FUNC(bright_magenta)
+	DECL_COLOR_FUNC(bright_cyan)
+	DECL_COLOR_FUNC(bright_white)
+	#undef DECL_COLOR_FUNC
+}
 
 //------------------------------------------------------------------------------
 // constants
@@ -67,31 +143,48 @@ void Logger::log(LogLevel a_level, const char* a_format, ...)
 	va_end (args);	
 
 	// output prefix and timestamp
-	std::cerr << library_prefix << "[" << sys::get_timestamp() << "] ";
+	std::cerr << library_prefix << "[" << color::bright_black(sys::get_timestamp()) << "] ";
 
 	// output log level
+	std::string level;
+	color::Color fcol = color::Color::none;
+	color::Color bcol = color::Color::none;
+	color::Style style = color::Style::none;
 	switch (a_level) {
 	case eLogFatal:
-		std::cerr << "[fatal] ";
+		level = "fatal";
+		fcol = color::Color::bright_white;
+		bcol = color::Color::red;
+		style = color::Style::underline;
 		break;
 	case eLogError:
-		std::cerr << "[error] ";
+		level = "error";
+		fcol = color::Color::bright_red;
+		style = color::Style::bold;
 		break;
 	case eLogWarning:
-		std::cerr << "[warn]  ";
+		level = "warn ";
+		fcol = color::Color::yellow;
 		break;
 	case eLogInfo:
-		std::cerr << "[info]  ";
+		level = "info ";
+		fcol = color::Color::none;
 		break;
 	case eLogVerbose:
-		std::cerr << "[verb]  ";
+		level = "verb ";
+		fcol = color::Color::bright_black;
 		break;
 	default:
-		std::cerr << "[L" << a_level << "] ";
+		level = "L" + std::to_string(a_level);
+		fcol = color::Color::none;
 	}
 
-	// output logger name and message
-	std::cerr << "[" << m_name << "] " << message << std::endl;
+	// output log level
+	std::cerr << "[" << color::colorize(level, fcol, bcol, style) << "] ";
+	// output logger name
+	std::cerr << "[" << color::cyan(m_name) << "] ";
+	// output message
+	std::cerr << color::colorize(message, fcol, bcol, style) << std::endl;
 }
 
 
